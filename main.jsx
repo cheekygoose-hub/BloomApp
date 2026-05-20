bb3cb05de5649d8e78d4c5bb416e4390913d5384
cheekygoose-hub <bradg.parker@gmail.com>
2026-05-20 16:33:43 +1000

    commit v3

--- main.jsx
+++ main.jsx
@@ -1,1 +1,9 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import BloomApp from './bloom-app.jsx';
 
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BloomApp />
+  </StrictMode>
+);
